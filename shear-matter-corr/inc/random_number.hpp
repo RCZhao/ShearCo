857a91d1c1b68bf7f9902955d766e227d3cda07b
Rongchuan Zhao <rzhao@astro.uni-bonn.de>
2020-07-28 17:01:49 +0200

    change to the new random number generator

--- shear-matter-corr/inc/random_number.hpp
+++ shear-matter-corr/inc/random_number.hpp
@@ -9,22 +9,21 @@
 #ifndef random_number_hpp
 #define random_number_hpp
 
-#include <iomanip>
-#include <random>
-#include <chrono>
+// #include <iomanip>
+#include "structdefs.hpp"
 
-//get a seed for a random number
-void seed() ;
+// //get a seed for a random number
+// void seed() ;
 
-// create a random number in an intervall (a,b)
-double unifRand(double a, double b)   ;
+// // create a random number in an intervall (a,b)
+// double unifRand(double a, double b)   ;
 
-// create a random number modulus to an input number
-int GetRand(const unsigned int modulo) ;
+// // create a random number modulus to an input number
+// int GetRand(const unsigned int modulo) ;
 
-extern std::mt19937 random_generator;
-double unif_real_Rand(const double a, const double b);
-int    unif_int_Rand(const unsigned int n);
-double gaussian_Rand(const double mean, const double sigma);
+void            seed(const int thread_id)                               ;
+double          unif_real_Rand(const double a, const double b)          ;
+unsigned long   unif_int_Rand(const unsigned long n)                    ;
+double          gaussian_Rand(const double mean, const double sigma)    ;
 
 #endif /* random_number_hpp */
--- shear-matter-corr/src/random_number.cpp
+++ shear-matter-corr/src/random_number.cpp
@@ -8,51 +8,31 @@
 
 #include "random_number.hpp"
 
-
 //get a seed for a random number
-void seed()
+void seed(const int thread_id)
 {
-    srand( static_cast<unsigned int>(time(NULL)) ) ;
+    unsigned long time_since_epoch = RG::myclock::now().time_since_epoch().count();
+    unsigned long rand_seed = (time_since_epoch*thread_id)%1000000000000000;
+    RG::random_generator.seed(rand_seed);
 }
 
-// create a random number in an intervall (a,b)
-double unifRand(double a, double b)
-{
-    return (b-a)*rand() / double(RAND_MAX) + a ;
-}
-
-// create a random number modulus to an input number
-int GetRand(const unsigned int modulo)
-{
-    static int Init = 0 ;
-    int randnum ;
-    
-    if (Init == 0)
-    {
-        seed() ;
-        Init = 1 ;
-    }
-    
-    randnum = rand() % modulo ;
-    
-    return randnum;
-}
-
-
+// return a uniformaly distributed real number R \in [a, b).
 double unif_real_Rand(const double a, const double b)
 {
     std::uniform_real_distribution<double> randU(a, b);
-    return randU(random_generator);
+    return randU(RG::random_generator);
 }
 
-int unif_int_Rand(const unsigned int n)
+// return a long int R \in [0, n) that follows the uniform discrete distribution.
+unsigned long unif_int_Rand(const unsigned long n)
 {
-    std::uniform_int_distribution<int> randU_int(0, n);
-    return randU_int(random_generator);
+    std::uniform_int_distribution<unsigned long> randU_int(0, n-1);
+    return randU_int(RG::random_generator);
 }
 
+// return a double that follows the Gaussian distribution with mean as the mean and sigma as the standard deviation
 double gaussian_Rand(const double mean, const double sigma)
 {
     std::normal_distribution<double> gaussian(mean, sigma);
-    return gaussian(random_generator);
+    return gaussian(RG::random_generator);
 }
